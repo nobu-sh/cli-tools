fd4b01c280fd8980b79d3d5f34c73abddf0de67d
Nobu <chat@nobu.sh>
2023-03-30 14:48:57 -0500

    wip: base

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-// -> This must come first
-// Registers all import aliases.
-import './aliases'
+#!/usr/bin/env node
 
-// -> This must come second
-// Injects all variables in .env file.
-import '@/env'
-
-// -> Test .env
-console.log(process.env.TEST, 'from .env')
+console.log('test');
